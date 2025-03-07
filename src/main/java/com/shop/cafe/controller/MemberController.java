bfdf18d50ed2acd83de04a9af60e34b64a394b29
kimminseo <kimminseo@DESKTOP-SLI222R>
2025-03-07 16:14:47 +0900

    feat: login 완료

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.shop.cafe.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.cafe.dto.Member;
@@ -15,6 +19,26 @@ public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m) {
+		System.out.println(m);
+		Map<String, String> responseMap = new HashMap<>();
+		try {
+			m=memberService.login(m);
+			String nickname=m.getNickname();
+			if(m!=null && nickname!=null && !nickname.trim().equals("")) {
+				responseMap.put("nickname", nickname);
+			}else {
+				responseMap.put("msg", "다시 로그인 해주세요");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseMap.put("msg", "다시 로그인 해주세요");
+		}
+		return responseMap;
+
+	}
 	
 	@PostMapping("insertMember")
 	public String insertMember(@RequestBody Member m) {
